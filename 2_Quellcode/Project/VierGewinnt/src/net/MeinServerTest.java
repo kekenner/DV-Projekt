d723c8391be12766b6e7888183109b8b162300cc
kekenner <kevin.roger.kenner@hs-furtwangen.de>
2022-06-26 22:35:09 +0200

    Vereinfachte Server Client Version. ~Marven Schwarz, Kevin Kenner
    Die ursprünglichen Server Client Klassen würden überarbeitet und vereinfacht. Dabei wurde der Thread der in beiden Klassen in den Methoden send() und empfange() erzeugt wurde weggelassen, da die Methode jeweils nur einmal aufgerufen und ausgeführt wird und dabei kein parallel Prozess durch einen Thread erzeugt werden muss.
    
    Co-Authored-By: mschwarz28 <101582153+mschwarz28@users.noreply.github.com>

--- 2_Quellcode/Project/VierGewinnt/src/net/MeinServerTest.java
+++ 2_Quellcode/Project/VierGewinnt/src/net/MeinServerTest.java
@@ -49,55 +49,55 @@ public class MeinServerTest {
 			}
 	}
 	
-	public void send_old(String Spalte) {
-		Thread sender = new Thread(new Runnable() { // Hier werden Daten vom Benutzer (der Eingabe) gelesen und an den
-													// Client gesendet
-			
-			@Override
-			public void run() {
-
-					out.println(Spalte); // schreibt die Daten in den clientSocket
-					out.flush(); // sendet die Daten an den Client
-					System.out.println("Data send.");
-		
-			}
-		});
-		sender.start();
-	}
+//	public void send_old(String Spalte) {
+//		Thread sender = new Thread(new Runnable() { // Hier werden Daten vom Benutzer (der Eingabe) gelesen und an den
+//													// Client gesendet
+//			
+//			@Override
+//			public void run() {
+//
+//					out.println(Spalte); // schreibt die Daten in den clientSocket
+//					out.flush(); // sendet die Daten an den Client
+//					System.out.println("Data send.");
+//		
+//			}
+//		});
+//		sender.start();
+//	}
 	
 	public void send(String Spalte) {
 		out.println(Spalte); // schreibt die Daten in den clientSocket
 		out.flush(); // sendet die Daten an den Client
 	}
 	
-	public int empfange_old() {
-		
-		Thread receive = new Thread(new Runnable() {
-			String col;
-			
-			@Override
-			public void run() {
-				try {
-					col = in.readLine();
-					while (col != null) {
-						//System.out.println("Client: " + msg);
-						col = in.readLine();
-						spalte = Integer.parseInt(col);
-						System.out.println(spalte);
-					}
-					System.out.println("Client ist nicht mehr verbunden.");
-
-					// out.close();
-					// clientSocket.close();
-					// serverSocket.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		receive.start();
-		return spalte;
-	}
+//	public int empfange_old() {
+//		
+//		Thread receive = new Thread(new Runnable() {
+//			String col;
+//			
+//			@Override
+//			public void run() {
+//				try {
+//					col = in.readLine();
+//					while (col != null) {
+//						//System.out.println("Client: " + msg);
+//						col = in.readLine();
+//						spalte = Integer.parseInt(col);
+//						System.out.println(spalte);
+//					}
+//					System.out.println("Client ist nicht mehr verbunden.");
+//
+//					// out.close();
+//					// clientSocket.close();
+//					// serverSocket.close();
+//				} catch (IOException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//		receive.start();
+//		return spalte;
+//	}
 	
 	public int empfange() {
 
